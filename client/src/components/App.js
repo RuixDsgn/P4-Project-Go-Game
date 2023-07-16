2837c90e212f95a5cb4465cc461f7609021d137e
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-16 16:41:26 -0700

    installed react-router and material-ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   // Code goes here!
