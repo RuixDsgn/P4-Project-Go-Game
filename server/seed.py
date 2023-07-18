c8f3ec7ab2aa8a4e09e4b63778d55f379effe5e3
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-18 14:57:13 -0700

    created fake reviews

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
+from random import random, choice as rc
 
 # Remote library imports
 from faker import Faker
@@ -10,8 +10,24 @@ from faker import Faker
 from app import app
 from models import db
 
+fake = Faker()
+
+def create_reviews():
+    reviews = []
+    for _ in range(500):
+        review = {
+            "title": fake.title,
+            "author": fake.name(),
+            "rating": random.randint(1, 5),
+            "content": fake.paragraph()
+        }
+        reviews.append(review)
+
+    return reviews
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+
         print("Starting seed...")
         # Seed code goes here!
