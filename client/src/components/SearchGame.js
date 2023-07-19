20c51d9d5cc02fe8ef6e9e123c00f9eb517bb550
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-19 12:07:45 -0700

    changed search button for better search slow

--- client/src/components/SearchGame.js
+++ client/src/components/SearchGame.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
 
 const { Search } = Input;
 
 function SearchGame({games, searchGames, setSearchGames}) {
-
-  const searchGameResults = games.filter((eachGame) => {
-    return eachGame.name.toLowerCase().includes(searchGames.toLowerCase())
-  })
 
   const handleGameSearch = e => {
     setSearchGames(e.target.value)
   }
 
   return (
-    <Search style={{width: '50%', float: 'right'}} size='middle' placeholder="search for games here" enterButton onChange={handleGameSearch}/>
+    <div>
+        <Input style={{width: '50%'}} defaultValue="Combine input and button" />
+        <Button type="primary" onClick={handleGameSearch}>Submit</Button>
+    </div>
 
   )
  
