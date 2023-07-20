e0089324adbbbec3b17fae3b2d1c701469477287
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-20 15:27:49 -0700

    created screenshot functionalities enabling users to hover over various screenshots,

--- client/src/components/GameProductPage.js
+++ client/src/components/GameProductPage.js
@@ -3,12 +3,14 @@ import {useParams} from 'react-router-dom'
 import GameReviewCard from './GameReviewCard'
 import GameCard from './GameCard'
 import { Row, Col, Card, Button } from 'antd'
+import { HeartOutlined } from '@ant-design/icons'
 
 const GameProduct = () => {
 
   const { id } = useParams();
   const [game, setGame] = useState({})
   const [isLoaded, setIsLoaded] = useState(false)
+  const [loadImg, setLoadImg] = useState('')
 
   useEffect(() => {
     fetch(`/games/product?id=${encodeURIComponent(id)}`)
@@ -17,13 +19,21 @@ const GameProduct = () => {
         setGame(gameData[0]);
         console.log(gameData[0]);
         setIsLoaded(true)
+        setLoadImg(gameData[0].cover.url)
       });
   }, []);
 
+  function showImg(e){
+    debugger;
+  }
 
   function renderScreenshots(){
     return game.screenshots.map((screenshot)=>{
-      return <img src={screenshot.url}/>
+      return (
+        <Col>
+           <img onMouseEnter={ (e) => {setLoadImg(e.target.src)}} style={{width: '50px', height: '50px', marginRight: '10px'}} src={screenshot.url}/>
+        </Col>
+      )
     })
   }
 
@@ -66,27 +76,42 @@ const GameProduct = () => {
 
   const generatedPrice = getRandomPrice()
   
+  function setRating(){
+    if (game.rating < 88) {
+      return '⭐️⭐️⭐️'
+    }
+    else if ( game.rating < 93 && game.rating >= 88) {
+      return '⭐️⭐️⭐️⭐️'
+    }
+    else if (game.rating >= 93) {
+      return '⭐️⭐️⭐️⭐️⭐️'
+    }
+  }
 
   return (
     isLoaded ? 
     <div style={{marginLeft: '50px'}}>
       <Row className='product-card'>
           <Col>
-              <img style={{width: '400px', marginRight: '50px'}} className='product-img' src={game.cover.url} alt="" />
+            {renderScreenshots()}
+          </Col>
+          <Col>
+              <img style={{width: '400px', marginRight: '50px'}} className='product-img' src={loadImg} alt="" />
           </Col>
               <div>
-                <p>title: {game.name}</p>
-                <p>genre(s): {mapGenres()}</p>
-                <p>platform(s): {mapPlatforms()}</p>
-                <p>rating</p>
-                <p>`Price: ${generatedPrice}`</p>
-                <Button type="primary">add to cart</Button>
+                <p><strong>Title:</strong> {game.name}</p>
+                <p><strong>Genre(s):</strong> {mapGenres()}</p>
+                <p style={{maxWidth: '500px'}}><strong>Platform(s):</strong> {mapPlatforms()}</p>
+                <p><strong>Rating: {setRating()}</strong></p>
+                <p><strong>$</strong>{generatedPrice}</p>
+                <Button style={{marginRight: '10px'}} type="primary">add to cart</Button>
+                <Button icon={<HeartOutlined />} href="" />
               </div>
       </Row>
 
       <div>
           <h4>Recent reviews from players</h4>
-          <GameReviewCard />
+          {/* <GameReviewCard /> */}
       </div>
       <br></br><br></br>
       <div>
